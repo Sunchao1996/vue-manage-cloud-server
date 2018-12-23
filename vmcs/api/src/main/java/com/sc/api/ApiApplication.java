235f588ffdd5a7263a01a9bd6bd2a0930627cf53
Schao <2061857679@qq.com>
2018-12-23 14:37:16 +0800

    去除多余依赖

--- vmcs/api/src/main/java/com/sc/api/ApiApplication.java
+++ vmcs/api/src/main/java/com/sc/api/ApiApplication.java
@@ -2,10 +2,11 @@ package com.sc.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @ComponentScan("com.sc")
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ApiApplication {
